7e5d62b5107c25e16ffe30433670aa2ab6dfc2cf
nstauffer <ngstauffer@gmail.com>
2021-08-18 11:17:15 -0700

    Add basic inputs

--- app.R
+++ app.R
@@ -1,28 +1,22 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
+#### UI ####
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Hot garbage"),
+    titlePanel("Indicator Calculator"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput(inputId = "search_type",
+                        label = "LDC search method",
+                        choices = c("Project Key", "Ecological Site ID"),
+                        selected = "Ecological Site ID"),
+            textInput(inputId = "search_string",
+                      label = "Search string",
+                      value = "",
+                      placeholder = "R036XB006NM")
         ),
 
         # Show a plot of the generated distribution
@@ -32,8 +26,8 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+#### SERVER ####
+server <- function(input, output, session) {
 
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
